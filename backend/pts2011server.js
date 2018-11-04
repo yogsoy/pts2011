23932be29fc775e77d81ccd3fa1b3062e8c53ef6
Julian Bennison <yogsy64@gmail.com>
2018-11-04 17:11:08 +1100

    now logs the request type

--- backend/pts2011server.js
+++ backend/pts2011server.js
@@ -1,7 +1,8 @@
 const http = require(`http`);
 
 http.createServer(function(req,res) {
-	console.log(req);
+	console.log(req.method);
+	console.log(req.headers);
 	res.write(`hello there`);
 	res.end();
 }).listen(981);

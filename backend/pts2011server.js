93a2292387c1bcb1ef963f2fa9aea508b08beb33
Julian Bennison <yogsy64@gmail.com>
2018-11-06 16:52:02 +1100

    added some fucking headers, how do you like those, fucker

--- backend/pts2011server.js
+++ backend/pts2011server.js
@@ -3,6 +3,10 @@ const http = require(`http`);
 http.createServer(function(req,res) {
 	console.log(req.method);
 	console.log(req.headers);
+	res.writeHead(200,{
+		"Hello":"there",
+		"Access-Control-Allow-Origin":"*"
+	});
 	res.write(`hello there`);
 	res.end();
 }).listen(981);

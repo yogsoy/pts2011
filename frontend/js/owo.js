c22518abcaf5dafb7c83be896ed7fabbc649db8d
Ethan Stubbs <onlinegamer748@gmail.com>
2018-11-07 04:15:49 +0000

    Added POST for sending chat in the future but nothing for now, just for testing at the moment.

--- frontend/js/owo.js
+++ frontend/js/owo.js
@@ -3,9 +3,23 @@ function loadStock() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("text").innerHTML = this.responseText;
+            data = JSON.parse(this.responseText);
+            document.getElementById("stock").innerHTML = "<strong>"+data.t+"</strong> | "+ data.m;
         }
     };
-    xhttp.open("GET", "http://14.2.161.34:981", true);
+    xhttp.open("GET", "http://14.2.161.34:981", true); //url for testing rn
+    xhttp.send();
+}
+function sendChat() {
+    //var data;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            //data = JSON.parse(this.responseText);
+            //document.getElementById("stock").innerHTML = "<strong>"+data.t+"</strong> | "+ data.m;
+        }
+    };
+    xhttp.open("POST", "http://127.0.0.1", true); //http://14.2.161.34:981
+    xhttp.setRequestHeader("atrax-protocol", 77)
     xhttp.send();
 }
